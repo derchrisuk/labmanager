0302797d697f220518a7cb58b169ba91f3b25d8d
Christian Gerbrandt <christian.gerbrandt@quantum.com>
2015-03-10 16:42:33 +0100

    Testing #sortable read/write SQLite

---
+++ dragtest.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>jQuery UI Draggable + Sortable</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+  <style>
+  ul { list-style-type: none; margin: 0; padding: 0; margin-bottom: 10px; }
+  li { margin: 5px; padding: 5px; width: 150px; }
+  </style>
+  <script>
+  
+$(document).ready(function(){
+    $('#sortable').sortable({
+        update: function() {
+            var stringDiv = "";
+            $("#sortable").children().each(function(i) {
+                var li = $(this);
+                stringDiv += " " + li.attr("name");
+            });
+
+            $.ajax({
+                type: "POST",
+                url: "updateOrder.php",
+                data: {order: stringDiv}
+            }); 
+        }
+    });
+    $( "#draggable" ).draggable({
+      connectToSortable: "#sortable",
+      revert: "true"
+    }); 
+    $( "#sortable" ).disableSelection();    
+});
+
+  </script>
+</head>
+<body>
+ 
+<ul>
+  <li id="draggable" class="ui-state-highlight" name="newsys1">Drag me down</li>
+</ul>
+
+<ul id="sortable">
+<?php
+require('inc/db.php');
+$db = new \labmanager\db();
+$devorder = $db->querySingle('SELECT deviceorder FROM racks WHERE id=1');
+$orderArray = explode(" ", $devorder);
+unset($orderArray[0]);
+foreach ($orderArray as $id) {
+	echo '<li class="ui-state-default" name="'. $id . '">'. $id . '</li>';
+}
+
+?> 
+</ul>
+ 
+ 
+</body>
+</html>
---
+++ updateOrder.php
@@ -0,0 +1,27 @@
+<?php
+include 'inc/ChromePhp.php';
+$debug = 0;
+$order = $_POST['order'];
+$split = "-----";
+ChromePhp::log($order);
+if ($debug==1) {
+	$debugfile = fopen("updateOrder.out", "a") or die("Unable to open file!");
+	fwrite($debugfile, $order);
+	fwrite($debugfile, $split);
+	fwrite($debugfile, $dump);
+	fwrite($debugfile, $split);
+	fwrite($debugfile, $orderArray);
+	fwrite($debugfile, $split);
+	fwrite($debugfile, var_dump($orderArray, true));
+	fclose($debugfile);
+}
+require('inc/db.php');
+$db = new \labmanager\db();
+ChromePhp::log('Query string: UPDATE racks set deviceorder="'. $order .'" WHERE id=1;');
+if ($db->query('UPDATE racks set deviceorder="'. $order .'" WHERE id=1;')) {
+	ChromePhp::log('ok');
+} else {
+	ChromePhp::log('fail');
+}
+ChromePhp::log($db->lastErrorMsg());
+?>
