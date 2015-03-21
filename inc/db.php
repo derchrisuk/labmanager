0396a03f27fe869acd7985c3f702749db12377c6
derchris <derchris@me.com>
2015-03-21 23:45:11 +0100

    Implement labs view

--- inc/db.php
+++ inc/db.php
@@ -32,4 +32,23 @@ class db extends \SQLite3
         $result->finalize();
         return $count;
     }
+
+    public function getCountIDAnd($tableID, $id, $value, $id2, $value2)
+    {
+        $result = $this->query('SELECT COUNT(*) AS count FROM ' . $tableID . ' WHERE ' . $id . '="' . $value . '" AND ' . $id2 . '="' . $value2 . '"');
+        $row = $result->fetchArray();
+        $count = $row[ 'count' ];
+        $result->finalize();
+        return $count;
+    }
+
+    public function getCountGroup($tableID, $id, $value, $group)
+    {
+        $result = $this->query('SELECT COUNT(*) AS count FROM ( SELECT * FROM ' . $tableID . ' WHERE ' . $id . '="' .
+            $value . '" GROUP BY ' . $group . ')');
+        $row = $result->fetchArray();
+        $count = $row[ 'count' ];
+        $result->finalize();
+        return $count;
+    }
 }
